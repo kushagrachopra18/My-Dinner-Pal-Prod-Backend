93e397891bdb8beafb143c5ee7290759d28e5fe6
Kushagra Chopra <Kushagra>
2022-04-14 14:48:42 -0400

    try bcryptjs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 
 require('dotenv').config();
