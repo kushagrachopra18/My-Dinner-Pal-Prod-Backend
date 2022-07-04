81428f05ef2c3b92898e84f2d39cd7765d69a81f
Kushagra Chopra <Kushagra>
2022-07-04 10:57:24 -0400

    Cleaned up comments

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-// app.use(cors({
-//   origin: ['https://mydinnerpal.com', 'http://localhost:8080', 'http://localhost:3000']
-// }));
-
 app.use(cors({
   origin: '*'
 }));
@@ -29,14 +25,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 // ----------SQL Connections-------
 var con = mysql.createPool({
